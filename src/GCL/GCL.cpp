e3c843657322e86d6321e672d6735fdbea9076e4
Matavilla <mr.andrej1102@yandex.ru>
2021-03-21 21:31:14 +0300

    fix and start for AVB

--- src/GCL/GCL.cpp
+++ src/GCL/GCL.cpp
@@ -1,6 +1,8 @@
 #include "GCL.h"
 
 uint64_t GCL::Period = 1;
+double GCL::IdleSlop = 0.65;
+double GCL::SendSlop = 0.35;
 
 uint64_t gcd(uint64_t a, uint64_t b) {
     uint64_t t;
@@ -22,7 +24,7 @@ void GCL::SetPeriod(const std::vector<Message>& MSG) {
     }
 }
 
-void eraseMsg(const Message& msg) {
+void GCL::eraseMsg(const Message& msg) {
     for (auto it = Sch.begin(); it != Sch.end();) {
         if (it->NumMsg == msg.Num) {
             it = Sch.erase(it);
@@ -32,7 +34,15 @@ void eraseMsg(const Message& msg) {
     }
 }
 
-bool addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time) {
+bool GCL::addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time) {
+    if (msg.Type == TypeMsg::TT) {
+        return addTTMsg(msg, time);
+    } else {
+        return addAVBMsg(msg, time);
+    }
+}
+
+bool GCL::addAVBMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time) {
     uint64_t numFrame = msg.Size / 1500;
     if (msg.Size % 1500) {
         numFrame += 1;
@@ -40,10 +50,16 @@ bool addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time
     double timeForTransfer = ((double) numFrame * 42 + msg.Size) / (Link_->Bandwidth);
 
 
+    bool flag = Sch.empty();
     for (auto& itTime : time) {
         uint64_t tIn = itTime.first;
-        uint64_t& tOut = it.Time.second;
+        uint64_t& tOut = itTime.second;
         tOut = timeForTransfer;
+        if (flag) {
+            Sch.emplace_back(0, msg.Num, timeForTransfer, tIn);
+            tOut += tIn;
+            continue;
+        }
         for (auto it = Sch.begin(); it != Sch.end();) {
             uint64_t start = it->Offset;
             uint64_t end = start + it->Dur;
@@ -52,11 +68,7 @@ bool addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time
             } else {
                 if ((tIn + timeForTransfer) <= start) {
                     // вставляем в интервал, все круто.
-                    GCLNote tmp;
-                    tmp.NumQueue = 0;
-                    tmp.NumMsg = msg.Num;
-                    tmp.Dur = timeForTransfer;
-                    tmp.Offset = tIn;
+                    GCLNote tmp(0, msg.Num, timeForTransfer, tIn);
                     tOut += tIn;
                     Sch.insert(it, std::move(tmp));
                 } else {
@@ -73,11 +85,73 @@ bool addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time
                         it++;
                     }
 
-                    GCLNote tmp;
-                    tmp.NumQueue = numQ + 1;
-                    tmp.NumMsg = msg.Num;
-                    tmp.Dur = timeForTransfer;
-                    tmp.Offset = end;
+                    GCLNote tmp(numQ + 1, msg.Num, timeForTransfer, end);
+                    tOut += end;
+
+                    if (it == Sch.end()) {
+                        if ((Period - end) < timeForTransfer) {
+                            return false;
+                        }
+                        Sch.push_back(std::move(tmp));
+                    } else {
+                        if (endQueue) {
+                            // нет пустого промежутка до конца очереди
+                            return false;
+                        }
+                        Sch.insert(it, std::move(tmp));
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return true;
+}
+
+bool GCL::addTTMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time) {
+    uint64_t numFrame = msg.Size / 1500;
+    if (msg.Size % 1500) {
+        numFrame += 1;
+    }
+    double timeForTransfer = ((double) numFrame * 42 + msg.Size) / (Link_->Bandwidth);
+
+
+    bool flag = Sch.empty();
+    for (auto& itTime : time) {
+        uint64_t tIn = itTime.first;
+        uint64_t& tOut = itTime.second;
+        tOut = timeForTransfer;
+        if (flag) {
+            Sch.emplace_back(0, msg.Num, timeForTransfer, tIn);
+            tOut += tIn;
+            continue;
+        }
+        for (auto it = Sch.begin(); it != Sch.end();) {
+            uint64_t start = it->Offset;
+            uint64_t end = start + it->Dur;
+            if (end <= tIn) {
+                it++;
+            } else {
+                if ((tIn + timeForTransfer) <= start) {
+                    // вставляем в интервал, все круто.
+                    GCLNote tmp(0, msg.Num, timeForTransfer, tIn);
+                    tOut += tIn;
+                    Sch.insert(it, std::move(tmp));
+                } else {
+                    int numQ = it->NumQueue;
+                    it++;
+                    bool endQueue = false;
+                    while (it != Sch.end() && (it->Offset - end) < timeForTransfer) {
+                        if (it->NumQueue == 5) {
+                            endQueue = true;
+                            break;
+                        }
+                        end = it->Offset + it->Dur;
+                        numQ = std::max(numQ, it->NumQueue);
+                        it++;
+                    }
+
+                    GCLNote tmp(numQ + 1, msg.Num, timeForTransfer, end);
                     tOut += end;
 
                     if (it == Sch.end()) {
--- src/GCL/GCL.h
+++ src/GCL/GCL.h
@@ -13,16 +13,26 @@ struct GCLNote {
     uint64_t NumMsg;
     double Dur;
     double Offset;
+
+    GCLNote(int a = 0, uint64_t b = 0, double c = 0.0, double d = 0.0) : NumQueue(a), NumMsg(b), Dur(c), Offset(d) {}
 };
 
 struct GCL {
     static uint64_t Period;
+
+    static double IdleSlop;
+    static double SendSlop;
+
     Link* Link_;
     std::list<GCLNote> Sch;
 
     static void SetPeriod(const std::vector<Message>& MSG);
 
     bool addMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time);
+
+    bool addTTMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time);
+
+    bool addAVBMsg(const Message& msg, std::vector<std::pair<uint64_t, uint64_t>>& time);
 
     void eraseMsg(const Message& msg); 
 };
